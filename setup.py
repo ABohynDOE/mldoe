9d264e22f25e60f0e4393f5fdbf447c476b385a6
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2021-04-13 16:17:37 +0200

    verbose in pytest in tox

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     # Package requirements for installation
     install_requires=[
         "numpy>=1.20",
-        'opackage>=2.6.8'
+        'oapackage>=2.6'
     ],
     # Package requirements for testing
     extras_require={
