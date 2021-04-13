10f21c796c671d51a287424de2ae95206cf11c76
ABohynDOE <alexandre.bohyn@kuleuven.be>
2021-04-13 23:10:05 +0200

    new design class name

--- test/test_design.py
+++ test/test_design.py
@@ -2,8 +2,9 @@ import mldoe
 
 
 def test_design_init():
-    assert(mldoe.TLdesign(16, [1, 2, 4, 8]).n_runs == 16)
-    assert(mldoe.MLdesign(16, [[1, 2, 3]], [4, 8]).n_runs == 16)
+    assert (mldoe.TLdes(16, [1, 2, 4, 8]).n_runs == 16)
+    assert (mldoe.MLdes(16, [[1, 2, 3]], [4, 8]).n_runs == 16)
+
 
 def test_design_array():
     pass
